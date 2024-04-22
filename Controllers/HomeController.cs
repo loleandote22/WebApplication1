08d467a1c6d2d872c66e65c9a826894dec25e920
Alfonso Sebastián Simorte Ramos <117114417+loleandote22@users.noreply.github.com>
2024-04-22 16:34:22 +0200

    v

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -18,7 +18,17 @@ namespace WebApplication1.Controllers
             return View();
         }
 
-        public IActionResult Privacy()
+        public IActionResult Carta()
+        {
+            return View();
+        }
+
+        public IActionResult About()
+        {
+            return View();
+        }
+
+        public IActionResult Contact()
         {
             return View();
         }
